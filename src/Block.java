0489c97434f648682c59d0f901b9c571940dde5f
poko <shuta.shibue@gmail.com>
2023-11-05 15:22:18 -0600

    Implemented Block.java

--- src/Block.java
+++ src/Block.java
@@ -1,5 +1,16 @@
+import java.nio.ByteBuffer;
+import java.security.DigestException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class Block {
-    
+
+    /**
+     * Block class to store single transaction information
+     *
+     * @author Kevin Johanson, Shuta Shibue
+     */
+
     // Fields
     int num;
     int amount;
@@ -7,35 +18,88 @@ public class Block {
     long nonce;
     // Constructor
 
-    public Block(int num, int amount, Hash prevHash){
-
+    public Block(int num, int amount, Hash prevHash) {
+        this.num = num;
+        this.amount = amount;
+        this.prevHash = prevHash;
+        try {
+            mine();
+        } catch (NoSuchAlgorithmException | DigestException e) {
+            e.printStackTrace();
+        }
     }
 
-    public Block(int num, int amount, Hash prevHash, long nonce){
-
+    public Block(int num, int amount, Hash prevHash, long nonce) {
+        this.num = num;
+        this.amount = amount;
+        this.prevHash = prevHash;
+        this.nonce = nonce;
     }
 
-    public int getNum(){
+    public int getNum() {
         return num;
     }
 
-    public int getAmount(){
+    public int getAmount() {
         return amount;
     }
 
-    public long getNonce(){
+    public long getNonce() {
         return nonce;
     }
 
-    public Hash getPrevHash(){
+    public Hash getPrevHash() {
         return prevHash;
     }
 
-    public Hash getHash(){
-        
+    public Hash getHash() {
+        MessageDigest md;
+        try {
+            md = MessageDigest.getInstance("sha-256");
+            md.update(ByteBuffer.allocate(4).putInt(getNum()).array());
+            md.update(ByteBuffer.allocate(4).putInt(getAmount()).array());
+            if (getPrevHash() != null)
+                md.update(getPrevHash().toString().getBytes()); // Only when prev exists
+            md.update(ByteBuffer.allocate(8).putLong(nonce).array());
+
+            byte[] hashByte = md.digest();
+            Hash hashObj = new Hash(hashByte);
+
+            return hashObj;
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
-    public String toString(){
-        return "";
+    void mine() throws NoSuchAlgorithmException, DigestException {
+        MessageDigest md = MessageDigest.getInstance("sha-256");
+
+        try {
+            md.update(ByteBuffer.allocate(4).putInt(getNum()).array());
+            md.update(ByteBuffer.allocate(4).putInt(getAmount()).array());
+            if (getPrevHash() != null)
+                md.update(getPrevHash().toString().getBytes()); // Only when prev exists
+            for (long i = 0; i < Long.MAX_VALUE - 1; i++) {
+                MessageDigest mdCopy = (MessageDigest) md.clone();
+                mdCopy.update(ByteBuffer.allocate(8).putLong(i).array());
+                byte[] hash = md.digest();
+
+                Hash tmpHash = new Hash(hash);
+                if (tmpHash.isValid()) {
+                    this.nonce = i;
+                    return;
+                }
+            }
+
+        } catch (CloneNotSupportedException cnse) {
+            throw new DigestException("couldn't make digest of partial content");
+        }
+    }
+
+    public String toString() {
+        String str = "Block " + getNum() + " (Amount: " + getAmount() + ", Nonce: " + getNonce()
+                + ", prevHash: " + getPrevHash() + ", hash: " + getHash() + ")";
+        return str;
     }
 }
